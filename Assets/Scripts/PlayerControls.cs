c4ec2c2e51908159e3e6ad88cc67c395bab4dd94
tadrius <116924580+tadrius@users.noreply.github.com>
2022-12-01 09:52:57 -0600

    Add new Input System

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+
+    [SerializeField] InputAction movement;
+    [SerializeField] InputAction shoot;
 
     private static string Horizontal = "Horizontal";
     private static string Vertical = "Vertical";
@@ -14,11 +18,28 @@ public class PlayerControls : MonoBehaviour
         
     }
 
+    void OnEnable() {
+        movement.Enable();
+        shoot.Enable();
+    }
+
+    void OnDisable() {
+        movement.Disable();
+        shoot.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalAxis = Input.GetAxis(Horizontal);
-        float verticalAxis = Input.GetAxis(Vertical);
+        // USING NEWER INPUT SYSTEM
+        Vector2 movementValue = movement.ReadValue<Vector2>();
+        float horizontalAxis = movementValue.x;
+        float verticalAxis = movementValue.y;
+
+        // USING OLDER INPUT MANAGER
+        // float horizontalAxis = Input.GetAxis(Horizontal);
+        // float verticalAxis = Input.GetAxis(Vertical);
+
         Debug.Log("Horizontal Axis: " + horizontalAxis);
         Debug.Log("Vertical Axis: " + verticalAxis);
     }
