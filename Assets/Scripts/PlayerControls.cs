14ca85c6213f8b084632a44bb5dac8b098806165
tadrius <116924580+tadrius@users.noreply.github.com>
2022-12-02 19:25:32 -0600

    Add ship rotations
    Add additional post-processing
    Tweak player values

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -8,16 +8,19 @@ public class PlayerControls : MonoBehaviour
 
     [SerializeField] InputAction movement;
     [SerializeField] InputAction shoot;
-    [SerializeField] float xSpeed = 10.0f;
-    [SerializeField] float ySpeed = 10.0f;
-    [SerializeField] [Min(0.0f)] float xRange = 4.0f;
-    [SerializeField] [Min(0.0f)] float yRange = 4.0f;
+    [SerializeField] float translateSpeed = 7.5f;
+    [SerializeField] float positionRotateFactor = 7.5f;
+    [SerializeField] float controlRotateFactor = 15.0f;
+    [SerializeField] [Min(0.0f)] float xTranslateRange = 3.75f;
+    [SerializeField] [Min(0.0f)] float yTranslateRange = 2.25f;
+    [SerializeField] [Min(0.0f)] float smoothingSpeed = 3.5f;
 
+    float xMove = 0.0f, yMove = 0.0f;
+    float pitchSmoothing = 0.5f, rollSmoothing = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     void OnEnable() {
@@ -33,24 +36,89 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVal = movement.ReadValue<Vector2>();
-        float xMove = moveVal.x;
-        float yMove = moveVal.y;
+        UpdateMoveFields();
+        Translate();
+        Rotate();
+    }
 
-        float xOffset = xMove * xSpeed * Time.deltaTime;
-        float yOffset = yMove * ySpeed * Time.deltaTime;
+    private void UpdateMoveFields() {
+        Vector2 moveVal = movement.ReadValue<Vector2>();
+        xMove = moveVal.x;
+        yMove = moveVal.y;
+    }
+
+    private void Rotate() 
+    {
+
+        // calculate rotations based on position
+        float positionPitch = -transform.localPosition.y * positionRotateFactor;
+        float positionYaw = transform.localPosition.x * positionRotateFactor;
+        float positionRoll = 0.0f;
+
+        // calculate rotations based on control
+        float minControlPitch = -controlRotateFactor;
+        float minControlRoll = -controlRotateFactor;
+        
+        float maxControlPitch = controlRotateFactor;
+        float maxControlRoll = controlRotateFactor;
+
+        pitchSmoothing = processSmoothing(yMove, pitchSmoothing);
+        rollSmoothing = processSmoothing(xMove, rollSmoothing);
+
+        float controlPitch = Mathf.Lerp(minControlPitch, maxControlPitch, pitchSmoothing);
+        float controlYaw = 0.0f;
+        float controlRoll = Mathf.Lerp(minControlRoll, maxControlRoll, rollSmoothing);
+
+        // apply rotation
+        transform.localRotation = Quaternion.Euler(
+            positionPitch - controlPitch, 
+            positionYaw - controlYaw, 
+            positionRoll - controlRoll);
+    }
+
+    private float processSmoothing(float control, float curSmoothing) {
+        float deltaSmoothingSpeed = smoothingSpeed * Time.deltaTime;
+        if (curSmoothing > 0.5f) {
+            if (control > 0.0f) {
+                curSmoothing = Mathf.Min(curSmoothing + deltaSmoothingSpeed, 1.0f);
+            } else if (control < 0.0f) {
+                curSmoothing = curSmoothing - 2.0f * deltaSmoothingSpeed;
+            } else {
+                curSmoothing = Mathf.Max(curSmoothing - deltaSmoothingSpeed, 0.5f);
+            }
+        } else if (curSmoothing < 0.5f) {
+            if (control > 0.0f) {
+                curSmoothing = curSmoothing + 2.0f * deltaSmoothingSpeed;
+            } else if (control < 0.0f) {
+                curSmoothing = Mathf.Max(curSmoothing - deltaSmoothingSpeed, 0.0f);
+            } else {
+                curSmoothing = Mathf.Min(curSmoothing + deltaSmoothingSpeed, 0.5f);
+            }
+        } else {
+            if (control > 0.0f) {
+                curSmoothing = Mathf.Min(curSmoothing + deltaSmoothingSpeed, 1.0f);
+            } else if (control < 0.0f) {
+                curSmoothing = Mathf.Max(curSmoothing - deltaSmoothingSpeed, 0.0f);
+            }            
+        }
+        return curSmoothing;
+    }
+
+    private void Translate()
+    {
+        float xOffset = xMove * translateSpeed * Time.deltaTime;
+        float yOffset = yMove * translateSpeed * Time.deltaTime;
 
         float xPos = transform.localPosition.x + xOffset;
         float yPos = transform.localPosition.y + yOffset;
 
-        float clampedXPos = Mathf.Clamp(xPos, -xRange, xRange);
-        float clampedYPos = Mathf.Clamp(yPos, 0, yRange); // y min as 0 since ship starts at bottom of screen
+        float clampedXPos = Mathf.Clamp(xPos, -xTranslateRange, xTranslateRange);
+        float clampedYPos = Mathf.Clamp(yPos, -yTranslateRange, yTranslateRange);
 
         transform.localPosition = new Vector3(
-            clampedXPos, 
-            clampedYPos, 
+            clampedXPos,
+            clampedYPos,
             transform.localPosition.z
         );
-
     }
 }
