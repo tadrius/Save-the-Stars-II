6833e933cbdc3e1c252030461529750ce9fec92a
tadrius <116924580+tadrius@users.noreply.github.com>
2022-12-19 15:37:06 -0600

    Add scoreboard UI
    Create additional prefabs

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,26 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
 
     private int score = 0;
+    private TMP_Text text;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        text = GetComponent<TMP_Text>();
+        text.text = $"SCORE: {score}";
     }
 
     public void IncreaseScore(int points) {
         score += points;
-        Debug.Log($"Score: {score}");
+        text.text = $"SCORE: {score}";
     }
 }
