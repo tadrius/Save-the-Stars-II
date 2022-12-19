44f892f647c92fa0dcb27a3a70bed27d6e37ddb4
tadrius <116924580+tadrius@users.noreply.github.com>
2022-12-19 14:38:14 -0600

    Add scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    [Tooltip("VFX for enemy death.")]
     [SerializeField] GameObject deathParticles;
+    [Tooltip("Parent object under which to spawn runtime objects such as deathParticles.")]
     [SerializeField] Transform runtimeSpawnsParent;
+    [Tooltip("Enemy point value. When enemy is destroyed the player's score will increase by this amount.")]
+    [SerializeField] int pointValue = 1;
 
+    Scoreboard scoreboard;
+
+    private void Start() {
+        scoreboard = FindObjectOfType<Scoreboard>();
+    }
 
     private void OnParticleCollision(GameObject other) {
-        // spawn death particles and child under runtime spawn parent
-        GameObject particlesInstance = Instantiate(deathParticles, transform.position, Quaternion.identity);
-        particlesInstance.transform.parent = runtimeSpawnsParent;
-
-        // destroy the enemy
+        scoreboard.IncreaseScore(pointValue);
+        SpawnDeathFX();
         Destroy(this.gameObject);
+    }
+
+    private void SpawnDeathFX() {
+         // spawn death particles and child under runtime spawn parent
+        GameObject particlesInstance = Instantiate(deathParticles, transform.position, Quaternion.identity);
+        particlesInstance.transform.parent = runtimeSpawnsParent;       
     }
 }
---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+
+    private int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseScore(int points) {
+        score += points;
+        Debug.Log($"Score: {score}");
+    }
+}
